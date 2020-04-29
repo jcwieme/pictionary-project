20c705b52886726143ec418012d40d98296f56e0
jcwieme <jc.wieme@outlook.com>
2020-04-29 10:34:24 +0200

    Setup server & client . side

---
+++ server.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const express = require('express')
+const socketIO = require('socket.io')
+
+const port = 3000
+const index = '/pictionary.html'
+
+const server = express()
+  .use((req, res) => {
+    res.sendFile(index, { root: __dirname })
+  })
+  .listen(port, () => console.log('Listening on port 3000'))
+
+const io = socketIO(server)
+
+io.on('connection', (socket) => {
+  console.log('A new client joined the server')
+
+  onConnection(socket)
+})
+
+function onConnection(socket) {
+  socket.on('username', (username) => {
+    console.log('Client name: ', username)
+  })
+
+  socket.on('line', (data) => {
+    socket.broadcast.emit('line', data)
+  })
+}
